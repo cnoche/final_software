b7fe9cf12acc8a94609743edaa1a29f52ba409bc
cnoche <54603928+cnoche@users.noreply.github.com>
2022-07-12 22:33:08 -0500

    fix sonar error

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-from db import *
+import db
 import sys
 
 def create_app():
-    app = Flask(__name__)
+    app = db.Flask(__name__)
     return app
     
 app = create_app()  
 
 @app.route("/publisher", methods=['POST'])
 def publish():
-    message = request.form["message"]
+    message = db.request.form["message"]
     print(message, file=sys.stderr)
-    topic = request.form["topic"]
+    topic = db.request.form["topic"]
     print(topic, file=sys.stderr)
-    newMes = Message(message, topic)
+    newMes = db.Message(message, topic)
     db.session.add(newMes)
     db.session.commit()
 
-    return render_template("templates/index.html")
+    return db.render_template("templates/index.html")
 
 @app.route("/subscriber/<topic>", methods=['GET'])
 def subs(topic=None):
     for result in db.engine.execute("SELECT * FROM Messages WHERE topic="+"'"+topic+"'"):
         print(result)
-    return render_template("templates/index.html")
+    return db.render_template("templates/index.html")
 
 if __name__ == "__main__":
     app.run(debug=False)
